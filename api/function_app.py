93c8855491eb7647d8c4b8d5400b1fd4f56e19c6
Christian Grier <christiangrier@Christians-MacBook-Pro.local>
2024-11-28 20:55:33 +1100

    updates for cloud deployment

---
+++ api/function_app.py
@@ -0,0 +1,30 @@
+import os
+import azure.functions as func
+from azure.cosmos import CosmosClient, exceptions
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+CONNECTION_STRING = os.environ['AzureCosmosDBConnectionString']
+COSMOS_CLIENT = CosmosClient.from_connection_string(conn_str=CONNECTION_STRING)
+DATABASE = COSMOS_CLIENT.get_database_client("AzureResume")
+CONTAINER = DATABASE.get_container_client("Counter")
+
+@app.function_name(name="HttpTrigger1")
+@app.route(route="http_trigger")
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    count = 0
+    query = f"SELECT * FROM c WHERE c.id = '1'" 
+    try:
+        for item in CONTAINER.query_items(query=query, enable_cross_partition_query=True):
+            updated_item = item
+            count = int(item['count'])
+            count += 1
+            updated_item['count'] = str(count)
+            CONTAINER.upsert_item(item, updated_item)
+    except exceptions.CosmosHttpResponseError as e:
+        logging.error(f"An error occurred: {e.status_code} - {e.message}")
+        count = e.message
+
+    return func.HttpResponse(body=str(count), status_code=200) 
--- frontend/js/count.js
+++ frontend/js/count.js
@@ -1,21 +1,20 @@
-window.addEventListener('DOMContentLoaded', (event) =>{
-    getVisitCount();
-})
-
-const functionApiUrl = '';
-
-const localFunctionApi = '';
-
-const getVisitCount = () => {
-    let count = 30;
-    fetch(functionApiUrl).then(response => {
-        return response.json()
-    }).then(response =>{
-        console.log("Website called function API.");
-        count =  response.count;
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error){
-        console.log(error);
+// Get the span element by its class name
+let span = document.getElementsByClassName("visit_count")[0];
+span.textContent = "Loading Count...";
+// Define a function to fetch the value from the endpoint
+function fetchValue() {
+  // Use the fetch API to make a GET request
+  fetch("http://localhost:7071/api/http_trigger")
+    .then(response => response.json()) // Parse the response as JSON
+    .then(data => {
+      // Update the span text with the data value
+      span.textContent = data;
+    })
+    .catch(error => {
+      // Handle any errors
+      console.error(error);
     });
-    return count;
 }
+
+// Call the function when the page loads
+fetchValue();
